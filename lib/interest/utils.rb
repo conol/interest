bfb07bc211e2b6ffa446fd8e067d239b18ca53ba
ta <ioiioioloo@gmail.com>
2014-11-02 18:13:12 +0900

    Use ActiveModel::Naming to name

--- lib/interest/utils.rb
+++ lib/interest/utils.rb
@@ -1,27 +1,39 @@
 require "active_support"
 require "active_support/core_ext/string/inflections"
 require "active_record"
+require "active_model/naming"
 
 module Interest
   module Utils
     class << self
       def symbolic_name_of(object)
-        if object.is_a?(ActiveRecord::Base)
-          object.class.name
-        elsif object.is_a?(Class) and object < ActiveRecord::Base
-          object.name
+        if model_class_or_instance(object)
+          ActiveModel::Naming.plural(object)
         else
-          object.to_s
-        end.underscore.pluralize
+          object.to_s.underscore.pluralize
+        end
       end
 
       def source_type_of(object)
-        if object.is_a?(ActiveRecord::Base)
-          object.class.name
-        elsif object.is_a?(Class) and object < ActiveRecord::Base
-          object.name
+        case model_class_or_instance(object)
+        when :instance
+          object.class.model_name.to_s
+        when :class
+          object.model_name.to_s
         else
           object.to_s.classify
+        end
+      end
+
+      private
+
+      def model_class_or_instance(object)
+        if object.is_a?(ActiveRecord::Base)
+          :instance
+        elsif object.is_a?(Class) && object < ActiveRecord::Base
+          :class
+        else
+          nil
         end
       end
     end
